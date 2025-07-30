9487f292aff2b67118b14277dc5791de9c5c7133
JIMENENDEZ25 <jmenendeza3@miumg.edu.gt>
2025-07-29 18:28:08 -0600

    PRIMER API BASE

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "ep-empty-dust-a4xw1aua-pooler.us-east-1.aws.neon.tech",
+  USER: "neondb_owner",
+  PASSWORD: "npg_bKk2a9dWfLSG",
+  DB: "neondb",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
